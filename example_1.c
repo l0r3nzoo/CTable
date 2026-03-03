9aff09814988f2ee93f1c4fc472b38852c2bcae8
bhanu <lorenbhanu@gmail.com>
2026-03-03 17:48:04 +0530

    some changes

--- example_1.c
+++ example_1.c
@@ -6,15 +6,17 @@ int main() {
   Table *table = table_init("example table");
   printf("table name : %s\n", table->TableName);
   if (add_column(table, "Id", INTEGER, PRIMARY_KEY, 1, NULL) != 0) {
-    printf("unable to add column");
+    printf("unable to add column\n");
     goto delete;
   }
   printf("no of columns in table %zu\n", table->NoOfColumms);
   if (add_row(table) != 0) {
-    printf("unable to add row");
+    printf("unable to add row\n");
     goto delete;
   }
 delete:
-  table_delete(&table);
+  if (table_delete(&table) != 0) {
+    puts("unable to delete table");
+  }
   return 0;
 }
